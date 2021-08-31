53dc49fa9a877606ccd8d2f9bc2d7e112f2841d6
David Bianchi <davidmb2@code.scs.illinois.edu>
2021-08-31 17:40:49 -0500

    Updated path to point to simulations directory

--- CME_ODE/program/in_out.py
+++ CME_ODE/program/in_out.py
@@ -374,7 +374,7 @@ def outMetCsvs(pmap,minute,procID):
     print("DF size on time: ",minute, " is: ", metsDF.size)
     print(metsDF)
 
-    metFileName = './sims/scan125-zan/rep-'+str(procID)+'.csv'#+'/'+str(minute)+'min-simDF_parts_end.csv'
+    metFileName = './simulations/batch1/rep-'+str(procID)+'.csv'#+'/'+str(minute)+'min-simDF_parts_end.csv'
     if (int(minute) == 0):
         metsDF.to_csv(metFileName,index=False,mode='w+')
     else:
