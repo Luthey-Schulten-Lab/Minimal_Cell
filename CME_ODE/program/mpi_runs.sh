801786ed511c6f04d217ccd70e0a4aa9f84cce6e
David Bianchi <davidmb2@code.scs.illinois.edu>
2021-09-11 16:20:28 -0500

    Example 5 minutes 5 replicate cells

--- CME_ODE/program/mpi_runs.sh
+++ CME_ODE/program/mpi_runs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ## Try 5 CME-ODE replicates for 5 minutes with a restart time of 1 minute
-mpirun -np 5 python3 mpi_wrapper.py -st cme-ode -t 3 -rs 1
+mpirun -np 5 python3 mpi_wrapper.py -st cme-ode -t 5 -rs 1
 
 #wait
